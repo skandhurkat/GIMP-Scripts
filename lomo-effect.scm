3c51a3c9f641a08ab74028d1c3593023ac737c3f
Skand Hurkat <skand_hurkat@yahoo.co.in>
2012-06-21 00:48:48 +0530

    Updated scripts. -Changed scripts to use local variables in accordance with the GIMP guidelines. -Added one click undo option to all scripts. -Added feature to reset foreground colour to all scripts which changed foreground colour.

--- lomo-effect.scm
+++ lomo-effect.scm
@@ -17,44 +17,71 @@
 ;An example script for the lomo effect.
 ;This script increases contrast on the red and green channels, decreases contrast on the blue channel, and increases saturation on the image. It also adds a Vignette.
 
-(define (script-fu-lomo-effect img drawable)
+(define (script-fu-lomo-effect 	img
+								drawable )
 
-        ;Create a new layer
-        (define curves-layer (car (gimp-layer-new-from-visible img img "Curves")))
+	;Set an undo group to undo the entire operation in one click
+	(gimp-image-undo-group-start img)
+  
+	;Define some local variables to use in this script.
+	;Please note that GIMP does not encourage global variables using "define", as that may lead to conflicts with other scripts.
+	(let* (
+			(original-foreground (car(gimp-context-get-foreground)))
+			(curves-layer (car (gimp-layer-new-from-visible img img "Curves")))
+			(vignette-group (car (gimp-layer-group-new img)))
+			(img-height (car (gimp-image-height img)))
+			(img-width (car (gimp-image-width img)))
+			(vignette-white)
+			(vignette-black))
+			
+		;And use the local variables here
+		
+		;Insert the curves-layer in the image
         (gimp-image-insert-layer img curves-layer 0 0)
 
         ;Modify the curves
         (gimp-curves-spline curves-layer HISTOGRAM-RED 8 #(0 0 60 30 190 220 255 255))
- (gimp-curves-spline curves-layer HISTOGRAM-GREEN 8 #(0 0 60 30 190 220 255 255))
- (gimp-curves-spline curves-layer HISTOGRAM-BLUE 8 #(0 0 30 60 220 190 255 255))
+		(gimp-curves-spline curves-layer HISTOGRAM-GREEN 8 #(0 0 60 30 190 220 255 255))
+		(gimp-curves-spline curves-layer HISTOGRAM-BLUE 8 #(0 0 30 60 220 190 255 255))
         ;Increase the saturation
         (gimp-hue-saturation curves-layer ALL-HUES 0 0 50)
 
         ;Now, add vignette layer group
- (define vignette-group (car (gimp-layer-group-new img)))
- (gimp-item-set-name vignette-group "Vignette")
- (gimp-layer-set-mode vignette-group OVERLAY-MODE)
- (gimp-layer-set-opacity vignette-group 100)
- (gimp-image-insert-layer img vignette-group 0 0)
-
- (define img-height (car (gimp-image-height img)))
- (define img-width (car (gimp-image-width img)))
+		(gimp-item-set-name vignette-group "Vignette")
+		(gimp-layer-set-mode vignette-group OVERLAY-MODE)
+		(gimp-layer-set-opacity vignette-group 100)
+		(gimp-image-insert-layer img vignette-group 0 0)
  
- ;Now, for the vignette layers
- (define vignette-white (car (gimp-layer-new img img-width img-height RGBA-IMAGE "White" 100 NORMAL-MODE)))
- (gimp-image-insert-layer img vignette-white vignette-group 0)
- (gimp-context-set-foreground '(255 255 255))
- (gimp-edit-blend vignette-white FG-TRANSPARENT-MODE NORMAL-MODE GRADIENT-RADIAL 100 0 REPEAT-NONE FALSE FALSE 1 0 TRUE (/ img-width 2) (/ img-height 2) (* img-width 1.5) (* img-height 1.5))
- (define vignette-black (car (gimp-layer-new img img-width img-height RGBA-IMAGE "Black" 100 NORMAL-MODE)))
- (gimp-image-insert-layer img vignette-black vignette-group 0)
- (gimp-context-set-foreground '(0 0 0))
- (gimp-edit-blend vignette-black FG-TRANSPARENT-MODE NORMAL-MODE GRADIENT-RADIAL 100 0 REPEAT-NONE TRUE FALSE 1 0 TRUE (/ img-width 2) (/ img-height 2) img-width img-height)
+		;Now, for the vignette layers
+		(set! vignette-white (car (gimp-layer-new img img-width img-height RGBA-IMAGE "White" 100 NORMAL-MODE)))
+		(gimp-image-insert-layer img vignette-white vignette-group 0)
+		(gimp-context-set-foreground '(255 255 255))
+		(gimp-edit-blend vignette-white FG-TRANSPARENT-MODE NORMAL-MODE GRADIENT-RADIAL 100 0 REPEAT-NONE FALSE FALSE 1 0 TRUE (/ img-width 2) (/ img-height 2) (* img-width 1.5) (* img-height 1.5))
+		(set! vignette-black (car (gimp-layer-new img img-width img-height RGBA-IMAGE "Black" 100 NORMAL-MODE)))
+		(gimp-image-insert-layer img vignette-black vignette-group 0)
+		(gimp-context-set-foreground '(0 0 0))
+		(gimp-edit-blend vignette-black FG-TRANSPARENT-MODE NORMAL-MODE GRADIENT-RADIAL 100 0 REPEAT-NONE TRUE FALSE 1 0 TRUE (/ img-width 2) (/ img-height 2) img-width img-height)
+		;Reset the foregound colour so that the user is none the wiser.
+		(gimp-context-set-foreground original-foreground))
 
-        ;Refresh the display
-        (gimp-displays-flush)
+	;Refresh the display
+	(gimp-displays-flush)
+	
+	;Finally, end the undo group
+	(gimp-image-undo-group-end img)
 )
 
-(script-fu-register "script-fu-lomo-effect" "Lomo Effect" "Apply a Lomo effect to visible image" "Skand Hurkat" "Skand Hurkat" "2012" "RGB*" SF-IMAGE "Image" 0 SF-DRAWABLE "Image to apply retro effect" 0)
+; Register the script so that GIMP may use it.
+(script-fu-register "script-fu-lomo-effect"
+   "Lomo Effect"
+   "Apply a Lomo effect to visible image"
+   "Skand Hurkat"
+   "© Skand Hurkat, 2012"
+   "June 21, 2012"
+   "RGB*"
+   SF-IMAGE "Image" 0
+   SF-DRAWABLE "Image to apply Lomo effect" 0)
 
-(script-fu-menu-register "script-fu-lomo-effect" "<Image>/Filters/Artistic")
-
+; This script will appear as a filter named "Lomo Effect" in the Filters > Photography block
+(script-fu-menu-register "script-fu-lomo-effect"
+   "<Image>/Filters/Photography")
--- retro-effect.scm
+++ retro-effect.scm
@@ -17,69 +17,86 @@
 ; Apply a retro effect to an image
 ; This is an example script only. Values must be changed to suit your tastes
 
-(define (script-fu-retro-effect img 
-    drawable )
- ;Create a new layer from the visible image
- (define curves-layer (car (gimp-layer-new-from-visible img img "Curves")))
+(define (script-fu-retro-effect 	img
+									drawable )
 
- ;Insert it in the image
- (gimp-image-insert-layer img curves-layer 0 0)
+	;Set an undo group to undo the entire operation in one click
+	(gimp-image-undo-group-start img)
+  
+	;Define some local variables to use in this script.
+	;Please note that GIMP does not encourage global variables using "define", as that may lead to conflicts with other scripts.
+	(let* (
+			(original-foreground (car(gimp-context-get-foreground)))
+			(curves-layer (car (gimp-layer-new-from-visible img img "Curves")))
+			(soft-light-layer)
+			(vignette-group (car (gimp-layer-group-new img)))
+			(img-height (car (gimp-image-height img)))
+			(img-width (car (gimp-image-width img)))
+			(vignette-white)
+			(vignette-black)
+			(tint-layer))
+		
+		;And use the local variables here
+		
+		;Insert the curves-layer into the image
+		(gimp-image-insert-layer img curves-layer 0 0)
+		;Modify the curves on the curves-layer
+		(gimp-curves-spline curves-layer HISTOGRAM-RED 8 #(0 0 61 41 187 207 255 255))
+		(gimp-curves-spline curves-layer HISTOGRAM-GREEN 8 #(0 0 50 57 195 217 255 255))
+		(gimp-curves-spline curves-layer HISTOGRAM-BLUE 4 #(0 40 255 215))
+		
+		;Soft light layer for added contrast
+		(set! soft-light-layer (car (gimp-layer-new-from-drawable curves-layer img)))
+		(gimp-item-set-name soft-light-layer "Soft Light")
+		(gimp-layer-set-mode soft-light-layer SOFTLIGHT-MODE)
+		(gimp-layer-set-opacity soft-light-layer 40)
+		(gimp-image-insert-layer img soft-light-layer 0 0)
+		(gimp-desaturate-full soft-light-layer DESATURATE-LUMINOSITY)
 
- ;Modify the curves
- (gimp-curves-spline curves-layer HISTOGRAM-RED 8 #(0 0 61 41 187 207 255 255))
- (gimp-curves-spline curves-layer HISTOGRAM-GREEN 8 #(0 0 50 57 195 217 255 255))
- (gimp-curves-spline curves-layer HISTOGRAM-BLUE 4 #(0 40 255 215))
+		;Now, add vignette layer group
+		(gimp-item-set-name vignette-group "Vignette")
+		(gimp-layer-set-mode vignette-group OVERLAY-MODE)
+		(gimp-layer-set-opacity vignette-group 40)
+		(gimp-image-insert-layer img vignette-group 0 0)
 
- ;Soft light layer for added contrast
- (define soft-light-layer (car (gimp-layer-new-from-drawable curves-layer img)))
- (gimp-item-set-name soft-light-layer "Soft Light")
- (gimp-layer-set-mode soft-light-layer SOFTLIGHT-MODE)
- (gimp-layer-set-opacity soft-light-layer 40)
- (gimp-image-insert-layer img soft-light-layer 0 0)
- (gimp-desaturate-full soft-light-layer DESATURATE-LUMINOSITY)
+		;Now, for the vignette layers
+		(set! vignette-white (car (gimp-layer-new img img-width img-height RGBA-IMAGE "White" 100 NORMAL-MODE)))
+		(gimp-image-insert-layer img vignette-white vignette-group 0)
+		(gimp-context-set-foreground '(255 255 255))
+		(gimp-edit-blend vignette-white FG-TRANSPARENT-MODE NORMAL-MODE GRADIENT-RADIAL 100 0 REPEAT-NONE FALSE FALSE 1 0 TRUE (/ img-width 2) (/ img-height 2) (* img-width 1.5) (* img-height 1.5))
+		(set! vignette-black (car (gimp-layer-new img img-width img-height RGBA-IMAGE "Black" 100 NORMAL-MODE)))
+		(gimp-image-insert-layer img vignette-black vignette-group 0)
+		(gimp-context-set-foreground '(0 0 0))
+		(gimp-edit-blend vignette-black FG-TRANSPARENT-MODE NORMAL-MODE GRADIENT-RADIAL 100 0 REPEAT-NONE TRUE FALSE 1 0 TRUE (/ img-width 2) (/ img-height 2) img-width img-height)
 
- ;Now, add vignette layer group
- (define vignette-group (car (gimp-layer-group-new img)))
- (gimp-item-set-name vignette-group "Vignette")
- (gimp-layer-set-mode vignette-group OVERLAY-MODE)
- (gimp-layer-set-opacity vignette-group 40)
- (gimp-image-insert-layer img vignette-group 0 0)
+		;Finally, set the tint layer
+		(set! tint-layer (car (gimp-layer-new img img-width img-height RGBA-IMAGE "Tint" 20 DIFFERENCE-MODE)))
+		(gimp-image-insert-layer img tint-layer 0 0)
+		(gimp-context-set-foreground '(5 10 150))
+		(gimp-edit-fill tint-layer FOREGROUND-FILL)
+		
+		;Reset the foregound colour so that the user is none the wiser.
+		(gimp-context-set-foreground original-foreground))
 
- (define img-height (car (gimp-image-height img)))
- (define img-width (car (gimp-image-width img)))
- 
- ;Now, for the vignette layers
- (define vignette-white (car (gimp-layer-new img img-width img-height RGBA-IMAGE "White" 100 NORMAL-MODE)))
- (gimp-image-insert-layer img vignette-white vignette-group 0)
- (gimp-context-set-foreground '(255 255 255))
- (gimp-edit-blend vignette-white FG-TRANSPARENT-MODE NORMAL-MODE GRADIENT-RADIAL 100 0 REPEAT-NONE FALSE FALSE 1 0 TRUE (/ img-width 2) (/ img-height 2) (* img-width 1.5) (* img-height 1.5))
- (define vignette-black (car (gimp-layer-new img img-width img-height RGBA-IMAGE "Black" 100 NORMAL-MODE)))
- (gimp-image-insert-layer img vignette-black vignette-group 0)
- (gimp-context-set-foreground '(0 0 0))
- (gimp-edit-blend vignette-black FG-TRANSPARENT-MODE NORMAL-MODE GRADIENT-RADIAL 100 0 REPEAT-NONE TRUE FALSE 1 0 TRUE (/ img-width 2) (/ img-height 2) img-width img-height)
- 
- ;Finally, set the tint layer
- (define tint-layer (car (gimp-layer-new img img-width img-height RGBA-IMAGE "Tint" 20 DIFFERENCE-MODE)))
- (gimp-image-insert-layer img tint-layer 0 0)
- (gimp-context-set-foreground '(5 10 150))
- (gimp-edit-fill tint-layer FOREGROUND-FILL)
-
- ;And flush the display so as to reflect the changes
- (gimp-displays-flush)
+	;And flush the display so as to reflect the changes
+	(gimp-displays-flush)
+	
+	;Finally, end the undo group
+	(gimp-image-undo-group-end img)
 )
 
 ; Register the script so that GIMP may use it.
 (script-fu-register "script-fu-retro-effect"
    "Retro Effect"
-   "Apply a retro effect to visible image"
+   "Apply a \"retro\" effect to visible image"
    "Skand Hurkat"
-   "Skand Hurkat"
-   "2012"
+   "© Skand Hurkat, 2012"
+   "June 21, 2012"
    "RGB*"
    SF-IMAGE "Image" 0
    SF-DRAWABLE "Image to apply retro effect" 0)
 
-; This script will appear as a filter named "Retro Effect" in the Filters > Artistic block
+; This script will appear as a filter named "Retro Effect" in the Filters > Photography block
 (script-fu-menu-register "script-fu-retro-effect"
-   "<Image>/Filters/Artistic")
+   "<Image>/Filters/Photography")
 
