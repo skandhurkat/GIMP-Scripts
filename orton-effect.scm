4901dfa2b0746ec86484e5234f9cad35bc409687
Skand Hurkat <skand_hurkat@yahoo.co.in>
2012-12-30 02:19:09 +0800

    Updated copyright information in all files

--- orton-effect.scm
+++ orton-effect.scm
@@ -1,5 +1,5 @@
 ;orton-effect.scm
-;© Skand Hurkat, 2012
+;Copyright 2012, Skand Hurkat
 
 ;    This program is free software: you can redistribute it and/or modify
 ;    it under the terms of the GNU General Public License as published by
